a296055066f764866652e469bd2f85da4b4be2c4
Matheus Alves <matheusalves.bmd@gmail.com>
2021-08-27 17:22:04 -0300

    Change the quantity when the button is clicked
    Add a function to change the quantity to a fixed value when the button
    is clicked.

--- app/src/main/java/com/example/android/appjustjava/MainActivity.java
+++ app/src/main/java/com/example/android/appjustjava/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.appjustjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /**
+     * This method display the given quantity value on the screen
+     */
+    private void display (int number) {
+        String str = String.valueOf(number);
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(str);
     }
 }
