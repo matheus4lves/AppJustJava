b8bb7462eed9addb937243908175959232bca4e9
Matheus Alves <matheusalves.bmd@gmail.com>
2021-08-30 18:26:19 -0300

    Create functions for the increment/decrement buttons
    Let the user change the quantity of coffee through the buttons. When the
    button is clicked, the quantity is changed and the updated quantity and
    value are displayed on the screen.

--- app/src/main/java/com/example/android/appjustjava/MainActivity.java
+++ app/src/main/java/com/example/android/appjustjava/MainActivity.java
@@ -9,33 +9,58 @@ import android.widget.TextView;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    private int quantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        quantity = 0;
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 4;
         display(quantity);
-        displayPrice(quantity * 5);
+        displayPrice(quantity);
     }
 
     /**
      * This method display the given quantity value on the screen
      */
-    private void display (int number) {
-        String str = String.valueOf(number);
+    private void display(int quantity) {
+        String str = String.valueOf(quantity);
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
         quantityTextView.setText(str);
     }
 
-    private void displayPrice(int number) {
+    private void displayPrice(int quantity) {
+        quantity *= 5;
         TextView priceTextView = findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(quantity));
+    }
+
+    /**
+     * This method is called when the plus button is clicked
+     *
+     * @param view
+     */
+    public void increment(View view) {
+        quantity++;
+        display(quantity);
+        displayPrice(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked
+     *
+     * @param view
+     */
+    public void decrement(View view) {
+        if (quantity == 0) return;
+        quantity--;
+        display(quantity);
+        displayPrice(quantity);
     }
 }
